b4e75eccabadc26192176900cf0424d849dd97f1
James Bruce <30735571+jaybruce1998@users.noreply.github.com>
2025-11-04 00:48:22 -0500

    Refactor Cell struct and remove comments

--- Cell.swift
+++ Cell.swift
@@ -1,18 +1,10 @@
-//
-//  Cell.swift
-//  Sudoku
-//
-//  Created by Ji Won Lee on 11/2/25.
-//
-
-
-    struct Cell {
-        let r: Int, c: Int
-        static func allCells() -> [Cell] {
-            var list = [Cell]()
-            for r in 0..<9 {
-                for c in 0..<9 { list.append(Cell(r: r, c: c)) }
-            }
-            return list
+struct Cell {
+    let r: Int, c: Int
+    static func allCells() -> [Cell] {
+        var list = [Cell]()
+        for r in 0..<9 {
+            for c in 0..<9 { list.append(Cell(r: r, c: c)) }
         }
+        return list
     }
+}
